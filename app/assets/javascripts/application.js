3b3b147c84bc6955941ba48c96f822aabf2e4c9f
Rajbeer Singh <srajbeer522@gmail.com>
2020-02-20 23:45:51 +0530

    Individual Charts against base currency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,133 @@
 //= require d3
 //= require currency_conversions
 //= require_tree
+
+// ajax call to fetch json data and draw charts
+var loadData = function(data, url){
+    $.ajax({
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        url: url,
+        dataType: 'json',
+        data: data,
+        success: function(data){
+            if( data != null){
+                var conversioRateArr = getCurrencyRates(data);
+                drawChartForEachCurrency(conversioRateArr);
+                drawChartForAllCurrency(conversioRateArr);
+            }else{
+                $('#chart').html("<p id='fail_call' class='alert alert-danger'> Something Went Wrong");
+            }
+        },
+        failure: function(result){
+            error();
+        }
+    });
+};
+function getCurrencyRates(data) {
+    var dateArray = Object.keys(data);
+    var xAxis = ['x'].concat(dateArray);
+    var currencies = Object.keys(data[Object.keys(data)[0]]);
+    var conversioRateArr = [xAxis];
+    for (var j = 0; j < currencies.length; j++){
+        var innerArray = [currencies[j]];
+        for (var i = 0; i < dateArray.length; i++){
+            innerArray.push(data[dateArray[i]][currencies[j]]);
+        }
+        conversioRateArr.push(innerArray);
+    }
+    return conversioRateArr
+}
+
+function drawChartForEachCurrency(conversioRateArr){
+    for(i = 0; i < conversioRateArr.length-1; i++){
+        var chartId= 'chart'+i;
+        $('#charts_div').append("<div id="+chartId+" > </div>");
+        var chart = c3.generate({
+            bindto: '#chart'+i+'',
+            data: {
+                x: 'x',
+                columns:[
+                    conversioRateArr[0],
+                    conversioRateArr[i+1]
+                ]
+            },
+            zoom:{
+                enabled: true
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    },
+                    label: 'Dates'
+                },
+                y:{
+                    label: 'Currency Rates'
+                }
+            }
+        });
+    }
+}
+
+function drawChartForAllCurrency(conversioRateArr) {
+    var Id = conversioRateArr.length;
+    var chartId= 'chart'+Id;
+    $('#charts_div').append("<div id="+chartId+" > </div>");
+    var chart = c3.generate({
+        bindto: '#chart'+Id+'',
+        data: {
+            x: 'x',
+            columns:
+            conversioRateArr
+        },
+        axis: {
+            x: {
+                type: 'timeseries',
+                tick: {
+                    format: '%Y-%m-%d'
+                },
+                label: 'Date'
+            },
+            zoom:{
+                enabled: true
+            },
+            y: {
+                tick: 3,
+                label: 'Currency Rates'
+            }
+        }
+    });
+}
+// fetch data on button click
+function calculateDates () {
+    var url = event.target.dataset.url;
+    var currency = $('#base_currency').val()
+    var startDate = $('#start_date').val();
+    var endDate = $('#end_date').val();
+    var data = {currency: currency, start_date: startDate, end_date: endDate};
+    loadData(data, url);
+}
+//Input validation before form submit
+$(document).on('input', '#start_date, #end_date', function () {
+    var startDate = $('#start_date').val();
+    var endDate = $('#end_date').val();
+    if (startDate && endDate && startDate <= endDate){
+        $('#submit-search-btn').removeClass('disable_custom');
+        $('.start_date_err').addClass('hidden');
+        $('.end_date_err').addClass('hidden');
+        $('.start_date_not_selected').addClass('hidden');
+    }else if(startDate && !endDate){
+        $('.end_date_err').removeClass('hidden');
+    }else if(!startDate && endDate){
+        $('.start_date_not_selected').removeClass('hidden');
+    }
+    if(startDate && endDate && startDate > endDate){
+        $('.end_date_err').addClass('hidden');
+        $('.start_date_not_selected').addClass('hidden');
+        $('.start_date_err').removeClass('hidden');
+        $('#submit-search-btn').addClass('disable_custom');
+    }
+});
+
